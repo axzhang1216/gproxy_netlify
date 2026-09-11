4790d38fb197c2994be96f529e049435379c765c
Aoxing <47006350+axzhang1216@users.noreply.github.com>
2026-09-11 14:28:13 +0800

    fix: load vendored v3 edge runtime from function root

--- edge-functions/gproxy.ts
+++ edge-functions/gproxy.ts
@@ -1,6 +1,6 @@
 import type { Context } from "@netlify/edge-functions"
-import init, { EdgeConfig, start } from "../pkg/gproxy_host_edge.js"
-import { wasmBase64 } from "./gproxy_wasm_inline"
+import init, { EdgeConfig, start } from "./_lib/gproxy_host_edge.js"
+import { wasmBase64 } from "./_lib/gproxy_wasm_inline.ts"
 
 let wasmReady: ReturnType<typeof init> | undefined
 let hostPromise: ReturnType<typeof start> | undefined
